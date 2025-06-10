017f16c88d50f4a112d39cd93df11ae2ea8f5744
TwigDemonTF <91436428+TwigDemonTF@users.noreply.github.com>
2025-06-10 18:30:23 +0200

    attempted a get request
    i tried making a get request to the c# api, which at first was complaining about the ssl certificate, but i got around that by disabling the ssl verification for the requests library

--- ReactorWebsite/website/views/main_views.py
+++ ReactorWebsite/website/views/main_views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from typing import Any
 
+import requests
 import os
 
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+
+# try:
+#     res = requests.get("http://localhost:5168/User/Details/1", verify=False)
+#     res = res.json()
+#     print(res["minecraftUsername"])
+# except requests.exceptions.RequestException as ex:
+#     return HttpResponse(f"Error fetching data: {ex}")
+
 def index(request) -> HttpResponse:
-    return render(request, 'website/main/index.html', context={})
+    context: dict[str, Any] = {}
+
+    return render(request, 'website/main/index.html', context=context)
